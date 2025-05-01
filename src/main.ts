bbc447e8664028b07248e0b57d6997239c73d167
Raquel de Sá <silva.raquel.de.sa@gmail.com>
2025-05-01 15:33:49 -0400

    feat: configuracao firebase

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  configureSwagger(app);
+  configureValidationPipe(app);
+  await app.listen(process.env.PORT ?? 3000);
+}
 
+function configureSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('UniFood API')
     .setDescription('Documentação da API do UniFood')
     .setVersion('1.0')
     .addTag('unifood')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(process.env.PORT ?? 3000);
 }
+
+function configureValidationPipe(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties not defined in DTOs
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are provided
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
+}
+
 bootstrap();
