fd28b8e131547bf74c68deba67dd5e0701a3ffd2
Raquel de Sa Silva <raquel.silva@conecthus.org.br>
2025-06-09 09:03:15 -0400

    fix: erros de build

--- src/cart/dtos/cart.dto.ts
+++ src/cart/dtos/cart.dto.ts
@@ -1,9 +1,10 @@
 export interface CartItem {
   productId: string;
   quantidade: number;
+  preco: number;
 }
 
 export interface CartData {
   items: CartItem[];
-  subtotal?:number;
+  subtotal?: number;
 }
